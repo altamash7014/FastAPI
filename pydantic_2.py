7866b1ef6d112d3c09836282c6ec05676e0c2bd1
altamash7014 <altamash7014@gmail.com>
2026-03-01 12:39:24 +0530

    Add files via upload

--- pydantic_2.py
+++ pydantic_2.py
@@ -20,4 +20,11 @@ def insert_patient(patient: Patient):
     print(patient.age)
     print("Inserted patient pydantic data successfully ")
 
-insert_patient(patient1)    
+# now we can create multiple patient objects with different data and insert them without worrying about type validation and data validation as pydantic will take care of it.
+def update_patient(patient: Patient):
+    print(patient.name)
+    print(patient.age)
+    print("Updated patient pydantic data successfully ")
+
+insert_patient(patient1)  
+update_patient(patient1)  
